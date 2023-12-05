c3191d1ac3ef725c1948629d28663dc444d22498
joseale207 <jose.jimenez.h7@gmail.com>
2023-12-05 17:35:32 -0300

    modificacion casi completa, envio por las dudas para que prueben las rutas

--- js/script.js
+++ js/script.js
@@ -21,3 +21,43 @@ function restarItem() {
 buttonMas.addEventListener("click", aumentarItem);
 buttonMenos.addEventListener("click", restarItem);
 
+// vue.config.js
+module.exports = {
+    publicPath: '/',
+  };
+
+//const RUTA_DESTINO = 'http://localhost:8080/imagen_inventario/';
+const fs = require('fs');
+
+if (!fs.existsSync(RUTA_DESTINO)) {
+    fs.mkdirSync(RUTA_DESTINO);
+}
+
+const RUTA_DESTINO = './imagen_inventario/';
+
+// Ejemplo de código para guardar una imagen usando Node.js y Express
+const express = require('express');
+const multer = require('multer');
+
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, RUTA_DESTINO);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/subir-imagen', upload.single('imagen'), (req, res) => {
+    // Lógica adicional después de subir la imagen si es necesario
+    res.json({ mensaje: 'Imagen subida con éxito' });
+});
+
+// Resto de tu código
+app.listen(3000, () => {
+    console.log('Servidor web iniciado');
+});
--- pruebaBD.py
+++ pruebaBD.py
@@ -16,6 +16,7 @@ import time
 
 app = Flask(__name__)
 CORS(app)  # Esto habilitará CORS para todas las rutas
+
 #--------------------------------------------
 class Catalogo:
     
@@ -164,13 +165,13 @@ class Catalogo:
 
 
 catalogo = Catalogo(host='localhost', user='root', password='', database='paw_petproductos')
-#catalogo.agregar_producto(1, 'Pienso para perros', 'Pienso para perros adultos', 21000.99, '1kg', 10, 'eso.png', 'purina')
-#catalogo.agregar_producto(2, 'Pienso para gatos', 'Pienso para gatos', 13000.00, '1kg', 10, 'comida.png', 'purina')
+# catalogo.agregar_producto(1, 'Pienso para perros', 'Pienso para perros adultos', 21000.99, '1kg', 10, 'eso.png', 'purina')
+# catalogo.agregar_producto(2, 'Pienso para gatos', 'Pienso para gatos', 13000.00, '1kg', 10, 'comida.png', 'purina')
 # catalogo.agregar_producto(5, 'Jueguete para gatos', 'jueguete', 16000.00, 'Pequeño', 10, 'jueguete.png', 'petgasper')
 #catalogo.listar_productos()
 
 #Carpeta para guardar las imagenes.
-RUTA_DESTINO = './imagen_inventario/'
+RUTA_DESTINO = '../pages/imagen_inventario' 
 #--------------------------------------------------------------------
 @app.route("/productos", methods=["POST"])
 def agregar_producto():
@@ -217,38 +218,48 @@ def mostrar_producto(codigo):
 
 
 #--------------------------------------------------------------------
-@app.route("/productos/<int:codigo>", methods=["PUT"])
+@app.route("/productos/<int:codigo>", methods=["GET", "POST"])
 def modificar_producto(codigo):
-    #Recojo los datos del form
-    nuevo_nombre= request.form.get("nombre")
-    nueva_descripcion = request.form.get("descripcion")
-    nuevo_precio = request.form.get("precio")
-    nuevo_tamanio = request.form['tamanio']
-    nuevo_stock = request.form.get("stock")
-    imagen = request.files['imagen']
-    nuevo_proveedor = request.form.get("proveedor")
+    if request.method == "POST":
+        nuevo_nombre = request.form.get("nombre")
+        nueva_descripcion = request.form.get("descripcion")
+        nuevo_precio = request.form.get("precio")
+        nuevo_tamanio = request.form.get("tamanio")
+        nuevo_stock = request.form.get("stock")
+        nuevo_proveedor = request.form.get("proveedor")
 
-    # Procesamiento de la imagen
-    nombre_imagen = secure_filename(imagen.filename)
-    nombre_base, extension = os.path.splitext(nombre_imagen)
-    nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}"
-    imagen.save(os.path.join(RUTA_DESTINO, nombre_imagen))
+        # Procesar la imagen
+        if 'nuevaImagen' in request.files:
+            imagen = request.files['nuevaImagen']
+            if imagen and allowed_file(imagen.filename):
+                nombre_imagen = secure_filename(imagen.filename)
+                nombre_base, extension = os.path.splitext(nombre_imagen)
+                nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}"
+                ruta_imagen = os.path.join(app.config['UPLOAD_FOLDER'], nombre_imagen)
+                imagen.save(ruta_imagen)
+            else:
+                # Manejar el caso donde el archivo no es válido
+                pass
 
-    # Busco el producto guardado
-    producto = producto = catalogo.consultar_producto(codigo)
-    if producto: # Si existe el producto...
-        imagen= producto["imagen"]
-        # Armo la ruta a la imagen
-        ruta_imagen = os.path.join(RUTA_DESTINO, imagen)
+        # Luego, llamar a tu función modificar_producto con los datos procesados
+        catalogo.modificar_producto(codigo, nuevo_nombre, nueva_descripcion, nuevo_precio, nuevo_tamanio, nuevo_stock, nombre_imagen, nuevo_proveedor)
 
-        # Y si existe la borro.
-        if os.path.exists(ruta_imagen):
-            os.remove(ruta_imagen)
-    
-    if catalogo.modificar_producto(codigo, nuevo_nombre, nueva_descripcion, nuevo_precio, nuevo_tamanio, nuevo_stock, nombre_imagen, nuevo_proveedor):
-        return jsonify({"mensaje": "Producto modificado"}), 200
-    else:
-        return jsonify({"mensaje": "Producto no encontrado"}), 403
+        # Redirigir a alguna página después de la modificación
+        return redirect(url_for('producto_modificado', codigo=codigo))
+
+    # Lógica para manejar el método GET y mostrar la página con el formulario.
+    return render_template("tuplantilla.html", codigo=codigo)
+
+@app.route("/producto_modificado/<int:codigo>")
+def producto_modificado(codigo):
+    # Puedes renderizar una plantilla especial para indicar que el producto ha sido modificado.
+    # O simplemente redirigir a otra página.
+    return render_template("producto_modificado.html", codigo=codigo)
+
+# Función para verificar si una extensión de archivo es válida
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 
 #--------------------------------------------------------------------
@@ -271,7 +282,13 @@ def eliminar_producto(codigo):
     else:
         return jsonify({"mensaje": "Error al eliminar el producto"}), 500
     
+@app.route("/productos/confirmar_eliminar/<int:codigo>", methods=["POST"])
+def confirmar_eliminar_producto(codigo):
+    # Aquí puedes realizar cualquier lógica de confirmación necesaria
+    # Puedes verificar si el producto puede eliminarse y devolver una respuesta adecuada
 
+    # En este ejemplo, simplemente devolveremos un mensaje de confirmación
+    return jsonify({"mensaje": f"¿Estás seguro de que quieres eliminar el producto {codigo}?"}), 200
 #--------------------------------------------------------------------
 if __name__ == "__main__":
     app.run(debug=True)
